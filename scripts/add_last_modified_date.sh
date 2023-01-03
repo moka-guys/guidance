540d0d93c4ac33fc7ca251052d75581aa3512518
Rachel Duffin <rachel.g.duffin@gmail.com>
2023-01-03 17:43:42 +0000

    Fix timestamp update issues

--- scripts/add_last_modified_date.sh
+++ scripts/add_last_modified_date.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
 FILES="$(git ls-files docs)"
-MAXLEN=0
-IFS="$(printf "\n\b")"
+
 for f in $FILES; do
-    if [ ${#f} -gt $MAXLEN ]; then
-        MAXLEN=${#f}
+    file_modified_date=$(sed -n "/.*last_modified_date:.*/ p" "$f")
+    echo "$f: Modified date in file: $file_modified_date"
+    str="$(git log -1 --pretty=format:"%cd" --date=format:'%Y-%m-%d %H:%M:%S' "$f")"
+    echo "$f: Modified date in git log: $str"
+    if echo "$file_modified_date" | grep -q "$str"; then
+      echo "$f: File has not been modified"
+    else
+      datetime_now=$(date +'%Y-%m-%d %H:%M:%S')
+      printf "%s: Modified since last pull request: %s" "$f" "$str \n"
+      sed -i "s/.*last_modified_date:.*/last_modified_date: $datetime_now/" "$f"
     fi
 done
-for f in $FILES; do
-    str="$(git log -1 --pretty=format:"%cd" --date=format:'%Y-%m-%d %H:%M:%S' $f)"
-    sed -i "s/.*last_modified_date:.*/last_modified_date: $str/" "$f"
-    printf "%-${MAXLEN}s %s\n" "$f" "$str"
-done
+
 
