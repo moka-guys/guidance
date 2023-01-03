58726575fdef1b94cb36f9f6c11455e0b36b0a03
Rachel Duffin <rachel.g.duffin@gmail.com>
2023-01-03 18:41:19 +0000

    Change timestamp evaluation to time difference comparison

--- scripts/add_last_modified_date.sh
+++ scripts/add_last_modified_date.sh
@@ -4,16 +4,22 @@ FILES="$(git ls-files docs)"
 
 for f in $FILES; do
   echo "$f"
-    file_modified_date=$(sed -n "/.*last_modified_date:.*/ p" "$f")
+    file_modified_string=$(sed -n "/.*last_modified_date:.*/ p" "$f")
+    file_modified_date=$(echo "$file_modified_string" | sed 's/^last_modified_date: //')
     printf "\--- Modified date in file: %s \n" "$file_modified_date"
-    str="$(git log -1 --pretty=format:"%cd" --date=format:'%Y-%m-%d %H:%M:%S' "$f")"
-    printf "\--- Modified date in git log: %s \n" "$str"
-    if echo "$file_modified_date" | grep -q "$str"; then
-      printf "\--- File has not been modified \n"
-    else
+    gitlog_modified_date=$(git log -1 --pretty=format:"%cd" --date=format:'%Y-%m-%d %H:%M:%S' "$f")
+    printf "\--- Modified date in git log: %s \n" "$gitlog_modified_date"
+
+    diff=$(($(date -d "$file_modified_date" +%s)-$(date -d "$gitlog_modified_date" +%s)))
+    date_diff=$(date -d "@$diff" -u +'%H:%M:%S')
+    onemin=$(date -d 00:01:00 +'%H:%M:%S')]
+
+    if [ "$date_diff" > "$onemin" ]; then
       datetime_now=$(date +'%Y-%m-%d %H:%M:%S')
-      printf "\--- Modified since last pull request: %s \n" "$str"
+      printf "\--- Modified since last pull request \n"
       sed -i "s/.*last_modified_date:.*/last_modified_date: $datetime_now/" "$f"
+    else
+      printf "\--- File has not been modified \n"
     fi
 done
 
